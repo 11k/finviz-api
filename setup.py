406dc611beb83c65fe60001c91fec9735e081541
Farhad Jay <effejaye@gmail.com>
2018-12-12 11:29:18 -0800

    Fix syntax error

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="finviz_api",
-    description="A Finviz-backed screener for stock tickers."
+    description="A Finviz-backed screener for stock tickers.",
     install_requires=["beautifulsoup4", "requests"]
 )
